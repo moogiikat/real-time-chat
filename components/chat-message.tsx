3c32d80bad2460ebc2d0008b57b1fc5aff6e32a6
moogiikat <monkhoo1997ts@gmail.com>
2025-04-04 10:52:42 +0900

    refactor: add user icon

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils"
 import type { ChatMessage } from "@/hooks/use-realtime-chat"
+import { UserCircle2Icon } from "lucide-react"
 
 interface ChatMessageItemProps {
   message: ChatMessage
@@ -7,9 +8,15 @@ interface ChatMessageItemProps {
   showHeader: boolean
 }
 
-export const ChatMessageItem = ({ message, isOwnMessage, showHeader }: ChatMessageItemProps) => {
+export const ChatMessageItem = ({
+  message,
+  isOwnMessage,
+  showHeader,
+}: ChatMessageItemProps) => {
   return (
-    <div className={`flex mt-2 ${isOwnMessage ? "justify-end" : "justify-start"}`}>
+    <div
+      className={`flex mt-2 ${isOwnMessage ? "justify-end" : "justify-start"}`}
+    >
       <div
         className={cn("max-w-[75%] w-fit flex flex-col gap-1", {
           "items-end": isOwnMessage,
@@ -21,7 +28,10 @@ export const ChatMessageItem = ({ message, isOwnMessage, showHeader }: ChatMessa
               "justify-end flex-row-reverse": isOwnMessage,
             })}
           >
-            <span className={"font-medium"}>{message.user.name}</span>
+            <div className="flex items-center gap-2">
+              <UserCircle2Icon className="w-5 h-5" />
+              <span className={"font-medium"}>{message.user.name}</span>
+            </div>
             <span className="text-foreground/50 text-xs">
               {new Date(message.createdAt).toLocaleTimeString("en-US", {
                 hour: "2-digit",
@@ -34,7 +44,9 @@ export const ChatMessageItem = ({ message, isOwnMessage, showHeader }: ChatMessa
         <div
           className={cn(
             "py-2 px-3 rounded-xl text-sm w-fit",
-            isOwnMessage ? "bg-primary text-primary-foreground" : "bg-muted text-foreground",
+            isOwnMessage
+              ? "bg-primary text-primary-foreground"
+              : "bg-muted text-foreground"
           )}
         >
           {message.content}
@@ -43,4 +55,3 @@ export const ChatMessageItem = ({ message, isOwnMessage, showHeader }: ChatMessa
     </div>
   )
 }
-
